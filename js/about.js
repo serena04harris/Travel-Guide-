50d5bec4e8adc789c529d3a1becff0501940fb65
Serena Harris <sharri23@uvm.edu>
2024-12-08 10:27:01 -0500

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,1 +1,12 @@
+// Add a little interactivity just for fun
+document.addEventListener("DOMContentLoaded", () => {
+    const heroText = document.querySelector('.hero-text h1');
 
+    heroText.addEventListener('mouseover', () => {
+        heroText.style.color = '#ffcc66';
+    });
+
+    heroText.addEventListener('mouseout', () => {
+        heroText.style.color = '#004d66';
+    });
+});
